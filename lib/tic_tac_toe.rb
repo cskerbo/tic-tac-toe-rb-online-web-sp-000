22012650b783bb4f1b39dfbb20254d2fb1dcfac1
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-13 15:49:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -75,3 +75,24 @@ def turn(board)
     turn(board)
   end
 end
+
+def turn_count(board)
+  counter = 0
+  board.each do |turn|
+    if turn == "X" || turn == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
+
+def current_player(board)
+  player = nil
+  if turn_count(board) % 2 == 0
+    player = "X"
+  else
+    player = "O"
+  end
+  player
+end
